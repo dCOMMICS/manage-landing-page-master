097d89e12ef1a2b08f4ff328d3a71262b56670ef
dCOMMICS <sanwurld@gmail.com>
2024-01-27 20:38:15 +0300

    initial commit

---
+++ rng/js/index.js
@@ -0,0 +1,9 @@
+// require
+
+const annotate = RoughNotation.annotate;
+
+const annotationGroup = RoughNotation.annotationGroup;
+
+// method 1 single selection
+
+let e = document.querySelector('.content-1');
