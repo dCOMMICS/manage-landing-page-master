095554d710d7bd4e3825c7e22fd924becfcaf52e
dCOMMICS <sanwurld@gmail.com>
2024-01-27 21:02:18 +0300

    updated the javascript file

--- rng/js/index.js
+++ rng/js/index.js
@@ -7,3 +7,20 @@ const annotationGroup = RoughNotation.annotationGroup;
 // method 1 single selection
 
 let e = document.querySelector('.content-1');
+const annotation = annotate(e, {
+    type: 'underline'
+});
+
+// run 
+annotation.show();
+
+// method 2 group selection
+
+const g1 = annotate (document.querySelector('.content-1'), {
+    type: 'underline',
+    color:'##d9ed5d'
+});
+
+// run
+const  ag = annotationGroup([g1]);
+ag.show();
